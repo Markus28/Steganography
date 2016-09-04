3bffc84faac7dacfd587764892d18b0aa685c63c
Markus28 <montcyril@gmail.com>
2016-09-04 09:02:20 +0200

    Added audio to target video, AudioReader, AudioWriter

--- Steganography.py
+++ Steganography.py
@@ -12,6 +12,9 @@ import hashlib
 import random
 import cv2
 import time
+import numpy as np
+from scipy.io import wavfile
+import subprocess
 
 class AESCipher(object):
 
@@ -38,6 +41,90 @@ class AESCipher(object):
     def _unpad(s):
         return s[:-ord(s[len(s)-1:])]
 
+class AudioWriter(object):
+    def __init__(self, source, target):
+        self.source = source
+        self.target = target
+        
+    def write_txt(self, msg):
+        sample_rate, data = wavfile.read(self.source)
+        msg = "%d:%s"%(8*len(msg), msg)
+        msg = str_to_bytes(msg)
+        result = apply_bytes(data, msg)
+        wavfile.write(self.target, sample_rate, result)
+
+    def write_file(self, src_path, key=""):
+        print '[Write file]'
+        txt = file_to_txt(src_path)
+        txt = '{{{&&filename %s&&}}}%s'%(filename(src_path), txt)
+        self.write_encrypted_txt(txt, key)
+        print 'Done\n'
+
+    def write_encrypted_txt(self, txt_msg, key=""):
+        c = AESCipher(key)
+        print 'Encrypting...'
+        encrypted_msg = c.encrypt(txt_msg)
+        self.write_txt(encrypted_msg)
+
+
+class AudioReader(object):
+    def __init__(self, source):
+        self.source = source
+
+    def read_txt(self):
+        sample_rate, data = wavfile.read(self.source)
+        count = 0
+        receiving = False
+        msg = ''
+        encoded_byte = []
+        tot = 0
+        following = 0
+        received = 0
+        for i, encoded in enumerate(data):
+            if (i+1)%8 == 0:
+                tot = 0
+                for pos, bit in enumerate(encoded_byte):
+                    tot += (2**pos)*bit
+
+                encoded_byte = []
+                
+                if chr(tot) == ':' and not receiving:
+                    following = int(msg)
+                    receiving = True
+                    msg = ''
+                elif not receiving:
+                    msg += chr(tot)
+                elif received < following:
+                    received += 8
+                    msg += chr(tot)
+                else:
+                    return msg
+            else:
+                encoded_byte.append(last_bit(encoded))
+        return msg
+
+    def read_file_content(self, key=""):
+        txt = self.read_encrypted_txt(key)
+        if txt.find('{{{&&filename ') == -1:
+            raise Exception('The message found is no file')
+        else:
+            end = txt.find('&&}}}')
+            target_file_name = txt[len('{{{&&filename '):end]
+            encoded_content = txt[end+len('&&}}}'):]
+            return encoded_content, target_file_name
+    
+    def read_file(self, target_directory, key=""):
+        print '[Read file]'
+        encoded_content, target_file_name = self.read_file_content(key)
+        target_path = os.path.join(target_directory, target_file_name)
+        txt_to_file(encoded_content, target_path)
+        print 'Done\n'
+
+    def read_encrypted_txt(self, key=""):
+        encrypted_msg = self.read_txt()
+        c = AESCipher(key)
+        print 'Decrypting...'
+        return c.decrypt(encrypted_msg)
 
 class VideoReader(object):
     def __init__(self, source):
@@ -74,13 +161,12 @@ class VideoReader(object):
             chunked_pixels = chunks(pixels,8)
             print 'Looking for message...'
             for i, encoded_byte in enumerate(chunked_pixels):
-                c += 1
                 if c>max_info:
                     print 'Reached end, next frame...'
                     break
                 
-                for n, byte in enumerate(encoded_byte):
-                    tot += (2**n)*last_bit(byte)
+                for pos, byte in enumerate(encoded_byte):
+                    tot += (2**pos)*last_bit(byte)
                 if chr(tot) == ':' and not receiving:
                     try:
                         following = int(message)
@@ -98,7 +184,6 @@ class VideoReader(object):
                 else:
                     return message
                 tot = 0
-            c = 0
             srm('C:\Users\Markus\AppData\Local\Temp\\frame%d.png'%n)
         return message
 
@@ -166,15 +251,24 @@ class VideoWriter(object):
         print 'Creating Video, %d frames'%count
         img1 = cv2.imread('C:\Users\Markus\AppData\Local\Temp\\frame0.png')
         height , width , layers =  img1.shape
-        video = cv2.VideoWriter(self.target,-1,fps ,(width,height))
+        video = cv2.VideoWriter('C:\Users\Markus\AppData\Local\Temp\\steg_video.avi',-1,fps ,(width,height))
         print 'Collecting frames'
         for i in xrange(count-1):
             print 'C:\Users\Markus\AppData\Local\Temp\\frame%d.png'%i
             video.write( cv2.imread('C:\Users\Markus\AppData\Local\Temp\\frame%d.png'%i))
             srm('C:\Users\Markus\AppData\Local\Temp\\frame%d.png'%i)
-
+                        
         cv2.destroyAllWindows()
         video.release()
+
+        #TODO if fails, copy temporary file
+        b = subprocess.Popen('ffmpeg -i %s -ab 160k -ac 2 -ar 44100 -vn %s -y'%(self.source, 'C:\Users\Markus\AppData\Local\Temp\\steg_audio.wav'), stderr = subprocess.STDOUT, stdout = subprocess.PIPE, shell = True)
+        print b.stdout.read()
+        a = subprocess.Popen('ffmpeg -i %s -i %s -codec copy -shortest %s -y'%('C:\Users\Markus\AppData\Local\Temp\\steg_video.avi', 'C:\Users\Markus\AppData\Local\Temp\\steg_audio.wav', os.path.abspath(self.target)), stdout = subprocess.PIPE, stderr = subprocess.STDOUT, shell = True)
+        print a.stdout.read()
+        srm('C:\Users\Markus\AppData\Local\Temp\\steg_audio.wav')
+        srm('C:\Users\Markus\AppData\Local\Temp\\steg_video.avi')
+
 
     def write_file(self, src_path, key=""):
         print '[Write file]'
@@ -389,7 +483,7 @@ def srm(path, reps = 3):
         with open(path, 'wb') as f:
             f.write(os.urandom(l))
     os.remove(path)
-    
+
 
 if __name__ == '__main__':              #Do some testing
     try:
@@ -436,7 +530,33 @@ if __name__ == '__main__':              #Do some testing
                 raise Exception('Test Input not equal to output')
         print "Testing ImageWriter's delete function:"
         writer.delete_msg()
-        print 'Test passed!'
+        try:
+            reader.read_file('', 'pass')
+            sys.stderr.write('Could not delete message\n')
+        except:                                                     #It is supposed to fail
+            print 'Test passed!\n'
     except Exception as e:
         sys.stderr.write('Test failed with exception: %s\n'%(e))
-        
+
+
+    try:
+        print 'Testing AudioWriter'
+        writer = AudioWriter('alcohol.wav', 'altered_media/alcohol_altered.wav')
+        writer.write_file('quote.rar', 'pass')
+        print 'Testing AudioReader'
+        reader = AudioReader('altered_media/alcohol_altered.wav')
+        reader.read_file('test_outputs', 'pass')
+        with open('quote.rar', 'r') as f:
+            archive = f.read()
+        while not os.path.isfile('test_outputs/quote.rar'):
+            count += 1
+            time.sleep(1)
+            if count>10:
+                count = 0
+                raise Exception('No Output was created')
+        with open('test_outputs/quote.rar') as f:
+            if not f.read() == archive:
+                raise Exception('Test Input not equal to output')
+        print 'Test passed!\n'
+    except Exception as e:
+        sys.stderr.write('Test failed with exception: %s\n'%(e))
